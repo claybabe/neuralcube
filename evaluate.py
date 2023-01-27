ee496105502675f600df83e10024a0287d136f68
theOGbaber <baberlicious@gmail.com>
2023-01-26 23:19:36 -0800

    action masking to prevent cycles. demo models.

--- evaluate.py
+++ evaluate.py
@@ -2,10 +2,32 @@
 
 from cube import Cube
 from model import BrownianAntipodalNavigator
-from torch import Generator, randint, tensor, argmax
+from torch import Generator, randint, tensor, argmax, rand, ones
 from torch.nn.functional import one_hot
 from tkinter import Tk, filedialog
 import torch
+
+def getChoice(pig, pigstory, sample):
+    turtle = Cube()
+    checks = 0
+    for choice in getChoices(sample):
+        turtle.setState(pig)
+        turtle.act(choice)
+        if turtle.getState() in pigstory:
+            checks += 1
+        else:
+            return choice, checks
+    return False, checks
+
+def getChoices(sample):
+    n = tensor(18)
+    mask = ones(n)
+    guesses = []
+    for _ in range(int(n)):
+        guess = argmax(sample * mask)
+        guesses.append(int(guess))
+        mask += one_hot(guess, n) * -1
+    return guesses
 
 if __name__ == "__main__":
     torch.manual_seed(0)
@@ -24,40 +46,47 @@ if __name__ == "__main__":
     generator = Generator()
     generator.manual_seed(12345678)
 
-    results = {"solved": 0, "cyclefail": 0, "timeout": 0, "longest": 0}
+    results = {"solved": 0, "cyclefail": 0, "timeout": 0, "longest": 0, "checks": 0}
     choices = {}
-    for i in range(100):
+    for i in range(42):
         cube.act(int(randint(0, 17, (1,), generator=generator)))
         pig = Cube()
         pig.setState(cube.getState())
         start = pig.toColorHot()
         history = set()
-        history.add(pig.getState())
         broke = False
         length = 0
-        for j in range(20):
+        actions = []
+        for j in range(42):
             length += 1
+            history.add(pig.getState())
+
             x = tensor(start + pig.toColorHot() + goal).float()
             a = tensor([0]*18).float()
             for model in models:
                 a += model(x)
-            x = int(argmax(a))
+
+            x, checks = getChoice(pig.getState(), history, a)
+            results["checks"] += checks
+            if x is False:
+                results["cyclefail"] += 1
+                print("cyclefail")
+                broke = True
+                break
             if x not in choices:
                 choices[x] = 0
             choices[x] += 1
             pig.act(x)
-            if pig.getState() in history:
-                results["cyclefail"] += 1
-                broke = True
-                break
+            actions.append(x)
             if pig.isSolved():
                 broke = True
                 break
-            history.add(pig.getState())
         if pig.isSolved():
             results["solved"] += 1
         else:
             if not broke:
                 results["timeout"] += 1
+                print("timeout")
         results["longest"] = max(results["longest"], length)
+        print(actions, "\n", pig)
     print(results, choices)
